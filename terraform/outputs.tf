0297b4dc818bebcb4ebd89d35223342e5905742e
Albert <albertmenendezg@gmail.com>
2025-07-14 19:57:33 +0200

    Removed duplicated outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,16 +20,6 @@ output "vm_public_ip_address" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
 
-output "private_ssh_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
-
-output "public_ssh_key" {
-  value     = tls_private_key.ssh_key.public_key_openssh
-  sensitive = true
-}
-
 
 output "vm_admin_username" {
   value = var.vm_admin_username
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vm-network" {
   name                = var.vm_virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
