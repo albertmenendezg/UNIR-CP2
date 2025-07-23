c8f7eed0d7466b257fa840fbd163137a904a3c57
Albert <albertmenendezg@gmail.com>
2025-07-23 22:01:22 +0200

    Removed inventory and movec to local_files.tf

--- terraform/local_files.tf
+++ terraform/local_files.tf
@@ -1,3 +1,16 @@
+resource "local_file" "ansible_inventory" {
+  depends_on = [azurerm_linux_virtual_machine.vm]
+  content = templatefile("inventory.tmpl", {
+    vm_ip             = azurerm_public_ip.vm_public_ip.ip_address
+    vm_admin_username = var.vm_admin_username
+    acr_username      = azurerm_container_registry.acr.admin_username
+    acr_password      = azurerm_container_registry.acr.admin_password
+    acr_name          = azurerm_container_registry.acr.name
+    ssh_key_path      = local_file.private_ssh_key.filename
+  })
+  filename = "${path.module}/../ansible/inventory"
+}
+
 resource "local_file" "private_ssh_key" {
   content         = tls_private_key.ssh_key.private_key_pem
   filename        = "${path.module}/../ansible/ssh_key.pem"
